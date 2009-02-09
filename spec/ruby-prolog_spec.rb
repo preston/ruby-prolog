139398ec4ee499be9b4510c4731b95e1deb4038a
Preston Lee <preston.lee@openrain.com>
2009-02-09 16:13:01 -0700

    fixing bug in "is" handling

--- spec/ruby-prolog_spec.rb
+++ spec/ruby-prolog_spec.rb
@@ -190,5 +190,33 @@ describe RubyProlog do
     
   end
   
+  
+  it 'should solve the Towers of Hanoi problem.' do
+    c = RubyProlog::Core.new
+    c.instance_eval do
+
+      move[0,:X,:Y,:Z] <<= :CUT   # There are no more moves left
+      move[:N,:A,:B,:C] <<= [
+        is(:M,:N){|n| n - 1}, # reads as "M IS N - 1"
+        move[:M,:A,:C,:B],
+        # write_info[:A,:B],
+        move[:M,:C,:B,:A]
+      ]
+      write_info[:X,:Y] <<= [
+        # write["move a disc from the "],
+        # write[:X], write[" pole to the "],
+        # write[:Y], writenl[" pole "]
+      ]
+
+       hanoi[:N] <<=  move[:N,"left","right","center"]
+       query(hanoi[5]).length.should == 1
+
+       # do_stuff[:STUFF].calls{|env| print env[:STUFF]; true}
+
+    end  
+    
+  end
+  
+  
 
 end
